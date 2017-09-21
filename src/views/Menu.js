5cb524a211d23b8c04f346855dda065211be6a16
Unknown <fbontin@gmail.com>
2017-09-21 20:40:35 +0200

    Menu bar changes

--- src/views/Menu.js
+++ src/views/Menu.js
@@ -2,18 +2,17 @@ var m = require("mithril")
 
 module.exports = {
     view: function(vnode) {
-        return m(".pure-g", 
+        return m(".pure-g menu-bar", 
             m(".pure-u-2-5", [
                 m(".name", "Filip Bontin"),
             ]), 
 
             m(".pure-u-3-5", 
                 m(".menu", 
-                    m("div", {class: "pure-menu pure-menu-horizontal"}, [
+                    m("div.pure-menu pure-menu-horizontal", [
                         m("ul.pure-menu-list", [
-                            m("li.pure-menu-item", m("a.pure-menu-link", {href: "#"}, "Home")),
-                            m("li.pure-menu-item", m("a.pure-menu-link", {href: "#"}, "About")),
-                            m("li.pure-menu-item", m("a.pure-menu-link", {href: "#"}, "Contact")),
+                            m("li.pure-menu-item", m("a.pure-menu-link menu-item", {href: "#", oncreate: m.route.link}, "Home")),
+                            m("li.pure-menu-item", m("a.pure-menu-link menu-item", {href: "/about", oncreate: m.route.link}, "About")),
                         ])
                     ])
                 )
