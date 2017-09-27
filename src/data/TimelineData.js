3e168b1e162146044ca6abc2870b3585aba9b973
Unknown <fbontin@gmail.com>
2017-09-27 21:33:01 +0200

    Add timeline data

---
+++ src/data/TimelineData.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        "title": "Valtech",
+        "text": "System developer.",
+        "startDate": "February 2017",
+        "endDate": "Current",
+    },
+    {
+        "title": "Master Thesis",
+        "text": "Developed a prototype for participatory city planning using Unity.",
+        "startDate": "May 2017",
+        "endDate": "October 2017",
+    },
+    {
+        "title": "Emmaboda Festival",
+        "text": "One part of a two-man team that developed the festival's first iPhone app.",
+        "startDate": "November 2016",
+        "endDate": "May 2017",
+    },
+    {
+        "title": "Exchange studies",
+        "text": "Graz University of Technology",
+        "startDate": "February 2016",
+        "endDate": "July 2016",
+    },
+]
